8c7c1ebf76241a19d3a0e397206c4f3572727e0c
marski84 <marski705@gmail.com>
2020-08-24 21:21:58 +0200

    solved

--- src/media/media.ts
+++ src/media/media.ts
@@ -1,24 +1,24 @@
 /*
-* Przeprogramowani.ts - https://przeprogramowani.pl/typescript/
-*
-* Media
-* ------------------
-*
-* Goal: At Przeprogramowani.pl we use multiple platforms at once - make sure that our favorite one works!
-* 
-* Hint: https://www.typescriptlang.org/docs/handbook/enums.html
-*/
+ * Przeprogramowani.ts - https://przeprogramowani.pl/typescript/
+ *
+ * Media
+ * ------------------
+ *
+ * Goal: At Przeprogramowani.pl we use multiple platforms at once - make sure that our favorite one works!
+ *
+ * Hint: https://www.typescriptlang.org/docs/handbook/enums.html
+ */
 
 enum Media {
-    Internet,
-    Press,
-    Radio
+  Internet = "Internet",
+  Press = "Press",
+  Radio = "Radio",
 }
 
-const przeprogramowani: Media = Media.Press;
+const przeprogramowani: Media = Media.Internet;
 
 /* Do not modify tests */
 
-test('should use proper media type', () => {
-    expect(przeprogramowani).toBe('Internet')
-})
+test("should use proper media type", () => {
+  expect(przeprogramowani).toBe("Internet");
+});
